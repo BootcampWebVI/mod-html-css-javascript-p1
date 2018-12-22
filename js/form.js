f7a14c877c1037d445557ead1c28a6d1cc41be1e
Pere Pico <isppicomuntaner@gmail.com>
2018-12-22 12:45:58 +0100

    Added form functionality. 'Other' message now only is only displayed when 'other' option is selected.

--- js/form.js
+++ js/form.js
@@ -1,5 +1,44 @@
-export function setForm (){
+export function setForm() {
 
-    
+    let form = document.querySelector('form')
+    let selector = form.querySelector('#selection')
+    let optionalMessage = document.querySelector('.optional_message')
+    let user = {}
+
+
+    form.addEventListener('submit', formController)
+    selector.addEventListener('change', optionalMessageController)
+
+    /* TODO Mostrar el mensaje opcional solamente cuando la opcion pertinente se encuentra seleccionada*/
+
+    function optionalMessageController(){
+        console.log(optionalMessage)
+        if(this.value == 'other'){
+            optionalMessage.classList.remove('hide')
+        } else {
+            optionalMessage.classList.add('hide')
+        }
+    }
+
+
+    /*TODO Cambiar el comportamiento del formulario para que los datos del usuario se impriman en la consola del navegador */
+
+    function formController(event) {
+        event.preventDefault()
+
+        user.name = document.querySelector('#name').value
+        user.email = document.querySelector('#email').value
+        user.howMetMe = getSelection(document.querySelector('#selection'))
+        user.other = document.querySelector('#other').value
+        user.tel = document.querySelector('#tel').value
+        user.msg = document.querySelector('#msg').value
+
+        console.table(user)
+
+    }
+
+    function getSelection(selector){
+        return selector[selector.selectedIndex].value
+    }
 
 }
