d01c8bd100dbe1b03c18353d470f5c75fbe7a4ec
Pere Pico <isppicomuntaner@gmail.com>
2018-12-22 13:42:19 +0100

    Added functionality. Now main message can't be longer than 150 words

--- js/form.js
+++ js/form.js
@@ -3,16 +3,30 @@ export function setForm() {
     let form = document.querySelector('form')
     let selector = form.querySelector('#selection')
     let optionalMessage = document.querySelector('.optional_message')
+    let mainTextArea = document.querySelector('#msg')
     let user = {}
 
 
-    form.addEventListener('submit', formController)
-    selector.addEventListener('change', optionalMessageController)
+    /* TODO controlar que la longitud del texto del mensaje principal es de menos de 150 palabras*/
+    mainTextArea.addEventListener('keydown', messageLengthController)
+
+    function messageLengthController(event){
+        const BACKSPACE_CODE = 8;
+        if (getTextAreaLengthInWords() > 5 && event.keyCode != BACKSPACE_CODE) {
+            event.preventDefault()
+        }
+    }
+
+    function getTextAreaLengthInWords(){
+        let wordsArray = mainTextArea.value.split(' ');
+        console.log(wordsArray.length)
+        return wordsArray.length
+    }
 
     /* TODO Mostrar el mensaje opcional solamente cuando la opcion pertinente se encuentra seleccionada*/
+    selector.addEventListener('change', optionalMessageController)
 
     function optionalMessageController() {
-        console.log(optionalMessage)
         if (this.value == 'other') {
             optionalMessage.classList.remove('hide')
         } else {
@@ -23,10 +37,10 @@ export function setForm() {
 
     /*TODO Cambiar el comportamiento del formulario para que los datos del usuario se impriman en la consola del navegador */
 
+    form.addEventListener('submit', formController)
+
     function formController(event) {
         event.preventDefault()
-
-        console.log(optionalMessage.classList.contains('hide'))
 
         user.name = document.querySelector('#name').value
         user.email = document.querySelector('#email').value
